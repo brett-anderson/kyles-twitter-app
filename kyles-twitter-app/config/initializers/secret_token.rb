8659e2a84fa17b8c0d82849e50413d4cea6e9a29
brett-anderson <brettanderson16@gmail.com>
2013-11-06 11:11:29 -0600

    rails project created

---
+++ kyles-twitter-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KylesTwitterApp::Application.config.secret_token = '53106db2388e6d059b38df5446c007bb6a920ebdafd8a6d5c06207ec2f2864bbdf50f1c0271022c38af041d504005cf46becd9fecc122fbae43846932646cf7f'
